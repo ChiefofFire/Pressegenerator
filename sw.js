7f375dbb790c64691acb5b20e30b15b3a0973197
ChiefofFire <satay-00aero@icloud.com>
2025-09-21 22:57:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'presse-v3';
+const CACHE = 'presse-v3'; // Version hochsetzen, wenn du Dateien änderst
 const ASSETS = ['.', 'index.html', 'script.js', 'manifest.json'];
 
 self.addEventListener('install', e=>{
@@ -6,9 +6,13 @@ self.addEventListener('install', e=>{
   self.skipWaiting();
 });
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
